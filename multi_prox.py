671c31868387bf70aee55135b7bd3f0ca0291c67
Lindsey <marralindseyj@gmail.com>
2018-11-17 20:07:19 +0000

    it's getting data from both sensors, but stopped working after a few minutes

--- multi_prox.py
+++ multi_prox.py
@@ -2,8 +2,13 @@ from smbus2 import SMBusWrapper
 
 from time import sleep
 
-
+##read from first sensor, wait, then read from second
 with SMBusWrapper(1) as bus:
+    bus.write_byte_data(0x70,0,1)
+    bus.write_byte_data(0x70,1,1)
     while 1:
-        data = bus.read_byte_data(0x70,0)
+        data = bus.read_byte(0x29,0)
         print(data)
+        data = bus.read_byte(0x29,1)
+        print(data)
+        sleep(.5)

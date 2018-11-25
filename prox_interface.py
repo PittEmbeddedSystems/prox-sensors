2238adc73fb7fa23738f50b7b8af6763c5ed902f
Lindsey <marralindseyj@gmail.com>
2018-11-25 11:36:34 -0500

    implements prox sensor interface

---
+++ prox_interface.py
@@ -0,0 +1,34 @@
+"""
+Implementation of interface to VL53L1X proximity sensor.
+"""
+
+import VL53L1X
+
+SENSOR_ADDRESS = 0x29
+I2C_BUS = 1
+prox_range = 1
+
+class ProxInterface(object):
+    """
+    Implementation of interface to sensor. For use with TCA9548A I2C switch, use sensor_switch first to select a sensor from the bus.
+    """
+    def __init__(self):
+        """
+        Initialize hardware interface.
+        """
+        self.prox = VL53L1X.VL53L1X(i2c_bus=I2C_BUS, i2c_address=SENSOR_ADDRESS)
+        
+    def read_prox(self):
+        """
+        Reads specified sensor with a predefined range.
+        1 = short range (used for PowerPlant application)
+        2 = medium range
+        3 = long range
+        
+        Returns distance between sensor and object in mm.
+        """
+        prox.open()
+        prox.start_ranging(prox_range)
+        return self.prox.read_prox().get.distance()
+        
+    

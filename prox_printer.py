1c6fab6615110f509fb5050d82be78c8c312bcb5
Lindsey <marralindseyj@gmail.com>
2018-11-25 11:41:27 -0500

    adds prox sensor use example

---
+++ prox_printer.py
@@ -0,0 +1,27 @@
+"""
+Use of sensor_switch and prox_interface to print proximity sensor measurements from the front and back sensors.
+"""
+from time import sleep
+from sensor_switch import MuxSwitcher
+from prox_interface import ProxInterface
+
+FRONT = 1
+BACK = 2
+
+def main():
+    bus = MuxSwitcher()
+    while True:
+        bus.sensor_select(FRONT)
+        prox = ProxInterface()
+        distance = prox.read_prox()
+        print("Front: {} mm".format(distance))
+        
+        bus.sensor_select(BACK)
+        prox = ProxInterface()
+        distance = prox.read_prox()
+        print("Front: {} mm".format(distance))
+        
+        sleep(0.5)
+        
+if __name__ == "__main__":
+    main()

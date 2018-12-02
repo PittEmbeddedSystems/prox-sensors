c15a62a7b2f71999d4675fafb35c94b05bcb9fea
Lindsey <marralindseyj@gmail.com>
2018-12-01 22:17:49 -0500

    Updates prox-sensor minor version to incorporate reliability fix.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='prox-sensors',
-      version='0.1',
+      version='0.3',
       description='library for reading prox outputs',
       py_modules=['prox_reader', 'prox_interface', 'sensor_switch']
     )
